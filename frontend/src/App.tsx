deb9dcb8fd3601c53dab365d586ce1725062c0dd
vitor <vitornt434@gmail.com>
2026-02-03 22:37:16 -0300

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-semibold text-blue-600">Seiwa Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
